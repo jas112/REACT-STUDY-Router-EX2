f64de6d6c68779db1c8ca443ce612ce98f44ca84
jas112 <jas.sandmaster@gmail.com>
2022-08-16 01:12:50 -0700

    router exercise 2 started... Starship and StarshipForm components started...

---
+++ src/components/starship/Starship.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Starship.css';
+
+class Starship extends Component {
+  render() {
+    return (
+      <div>Starship</div>
+    )
+  }
+}
+
+export default Starship;
---
+++ src/components/starshipForm/StarshipForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './StarshipForm.css';
+
+class StarshipForm extends Component {
+  render() {
+    return (
+      <div>StarshipForm</div>
+    )
+  }
+}
+
+export default StarshipForm;
