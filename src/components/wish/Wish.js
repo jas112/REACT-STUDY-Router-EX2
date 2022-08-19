ffa0feaea6063bab25cbf50a3632b21721c09af7
jas112 <jas.sandmaster@gmail.com>
2022-08-18 23:56:44 -0700

    added router redirect to Wish component.

--- src/components/wish/Wish.js
+++ src/components/wish/Wish.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 // import React from 'react';
 // import {useNavigate, useParams} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import './Wish.css';
 
 class Wish extends Component {
+
+  generateWish(wish, url){
+    return(
+      <div className='Wish-console'>
+        <h1 className='Wish-msg'>The object of your desire... {wish}</h1>
+        <div className='Wish-display'>
+          <img className='Wish-img' src={url} alt={wish} />
+        </div>
+      </div>
+    );
+  }
   render() {
 
     let wish = this.props.match.params.wish
     let targetUrl = `https://source.unsplash.com/1600x900/?${wish}}`;
 
+    let grantedWish = this.generateWish(wish, targetUrl);
+
     return (
       <div className='Wish'>
-        <h1 className='Wish-msg'>The object of your desire... {wish}</h1>
+        {/\d/.test(wish) ? <Redirect to='/' /> : grantedWish}
+        {/* <h1 className='Wish-msg'>The object of your desire... {wish}</h1>
         <div className='Wish-display'>
           <img className='Wish-img' src={targetUrl} alt={wish} />
-        </div>
+        </div> */}
       </div>
     )
   }
